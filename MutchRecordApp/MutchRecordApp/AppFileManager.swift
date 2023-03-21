b6fd5db351cd860860abd4e72a492ceeafb075e5
riku yamamoto <mikijuku114514@gmail.com>
2023-03-21 23:35:15 +0900

    不要部分削除

--- MutchRecordApp/MutchRecordApp/AppFileManager.swift
+++ MutchRecordApp/MutchRecordApp/AppFileManager.swift
@@ -21,26 +21,7 @@ class AppFileManager:NSObject{
             print("Error saveData")
         }
     }
-    
-    func readFromFile() -> String {
-        
-        /// ①DocumentsフォルダURL取得
-        guard let dirURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
-            fatalError("フォルダURL取得エラー")
-        }
-        
-        /// ②対象のファイルURL取得
-        let fileURL = dirURL.appendingPathComponent("output.txt")
-        
-        /// ③ファイルの読み込み
-        guard let fileContents = try? String(contentsOf: fileURL) else {
-            fatalError("ファイル読み込みエラー")
-        }
-        
-        /// ④読み込んだ内容を戻り値として返す
-        return fileContents
-    }
-    
+
     static func getSaveData() -> [String]{
         resultArray.removeAll()
         if let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
